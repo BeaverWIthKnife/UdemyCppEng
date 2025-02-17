e002cf1461d319f8adf1df9d174d725315aef3f2
Мединський Ярослав Анатолійович <beaver.with.knife@gmail.com>
2025-02-17 12:06:07 +0200

    Done
    Almost perfect, got error but it was resolved

--- Chapter02/Exercise/Exercise/AdFunctions.cc
+++ Chapter02/Exercise/Exercise/AdFunctions.cc
@@ -4,3 +4,32 @@
 
 #include "AdConstants.h"
 #include "AdFunctions.h"
+
+namespace Ad{
+    namespace Utils{
+        float kph_to_mps(const float kph){
+            return kph / 3.6F;
+        }
+    }
+    namespace Data{
+        Ad::Types::VehicleType init_ego_vehicle(){
+            Ad::Types::VehicleType ego_car{
+                .ID = Ad::Constants::EGO_VEHICLE_ID,
+                .Lane = Ad::Types::LaneAssociationType::Center,
+                .Speed = Ad::Utils::kph_to_mps(135.0F),
+                .Distance = 0.0F
+            };
+
+            return ego_car;
+        }
+    }
+
+    namespace Visualize{
+        void print_vehicle(const Ad::Types::VehicleType vehicle){
+            std::cout << "ID: " << vehicle.ID << '\n';
+        std::cout << "Speed (m/s): " << vehicle.Speed << '\n';
+        std::cout << "Distance (m): " << vehicle.Distance << '\n';
+        std::cout << "Lane: " << static_cast<std::int32_t>(vehicle.Lane) << '\n';
+        }
+    }
+}
--- Chapter02/Exercise/Exercise/AdTypes.h
+++ Chapter02/Exercise/Exercise/AdTypes.h
@@ -3,3 +3,21 @@
 #include <cstdint>
 
 #include "AdConstants.h"
+
+namespace Ad{
+    namespace Types{
+        enum class LaneAssociationType{
+            Unknown,
+            Left,
+            Right,
+            Center,
+        };
+
+        struct VehicleType{
+            std::int32_t ID;
+            Ad::Types::LaneAssociationType Lane;
+            float Speed;
+            float Distance;
+        };
+    }
+}
